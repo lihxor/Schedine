3c1baf3f34d71d7e93e7a6a390e928b3b262ff04
L0ll0 <lollo64.lv@gmail.com>
2021-03-27 16:58:04 +0100

    Inizio implementazione database per tenere in considerazione le ultime 5 partite di una squadra

--- modello.cpp
+++ modello.cpp
@@ -26,19 +26,19 @@ int diff_v=0;
 
 int partite_1[5]; //Risultati ultime 5 partite (Prima squadra)
 int partite_2[5]; //Risultati ultime 5 pratite (Seconda squadra)
-int vantaggio=0;
+float vantaggio=0;
 
 void compute(int a)
 {
   if(a<0)
   {
-    vantaggio--;
+    vantaggio+=(a)/10;
   }
   else
   {
     if(a>0)
     {
-      vantaggio++;
+      vantaggio+=(a)/10;
     }
     else
     {
@@ -100,18 +100,19 @@ int main()
   int gol_s_1=0; //Gol subiti dalla prima squadra
   int gol_f_2=0; //Gol fatti dalla seconda squadra
   int gol_s_2=0; //Gol subiti dalla seconda squadra
-  cout<<"Inserire classifica prima squadra"<<endl;
+  cout<<"Inserire classifica prima squadra"<<std::endl;
   cin>>val_1;
-  cout<<"Inserire gol fatti prima squadra"<<endl;
+  cout<<"Inserire gol fatti prima squadra"<<std::endl;
   cin>>gol_f_1;
-  cout<<"Inserire gol subiti prima squadra"<<endl;
+  cout<<"Inserire gol subiti prima squadra"<<std::endl;
   cin>>gol_s_1;
-  cout<<"Inserire classifica seconda squadra"<<endl;
+  cout<<"Inserire classifica seconda squadra"<<std::endl;
   cin>>val_2;
-  cout<<"Inserire gol fatti seconda squadra"<<endl;
+  cout<<"Inserire gol fatti seconda squadra"<<std::endl;
   cin>>gol_f_2;
-  cout<<"Inserire gol subiti seconda squadra"<<endl;
+  cout<<"Inserire gol subiti seconda squadra"<<std::endl;
   cin>>gol_s_2;
+
 
 
   /*
@@ -172,9 +173,9 @@ int main()
     tot_1+=squad_2[i];
   }
   */
-  cout<<"Valore squadra 1:"<<endl;
+  cout<<"Valore squadra 1:"<<std::endl;
   cin>>perfm_1;
-  cout<<"Valore squadra 2:"<<endl;
+  cout<<"Valore squadra 2:"<<std::endl;
   cin>>perfm_2;
   diff_v=val_1 - val_2;
   compute(diff_v);
