b4519efa4003e33f9639c52579be577fe129b329
lihxor <lollo64.lv@gmail.com>
2023-09-15 15:53:20 +0200

    Sincronizzazione

--- schedine.py
+++ schedine.py
@@ -2,10 +2,9 @@ import openpyxl
 import sys
 wb = openpyxl.load_workbook(".\Squadre.xlsx")
 ws = wb.active
-s = []
-offload = 3;n = 0; bil=0
+s = []; sq = []
+offset = 4;n = 0; bil=0
 payload = ""
-
 #--------------------------------------------------------------
 #Definizione della classe Squadra con i vari dati attribuiti
 class Squadra:
@@ -17,7 +16,6 @@ class Squadra:
         self.p = p
         self.g = g
         self.pt = pt
-
     def stampa(self):
         l = 11 - len(self.nome)
         out = self.nome; h = 0
@@ -63,37 +61,32 @@ except EOFError as e:
     print("Nessun valore inserito\n")
     sys.exit()
 print("\n")
-n = -1
-while n<2:
-    n = n + 1
-    i=0
-    while i < 21  :
-        i = i + 1
-        if(s[0] in str(ws.cell(i,2).value) and n == 0):
-            s1 = Squadra(str(ws.cell(i,2).value),
-            ws.cell(i,3).value,
-            ws.cell(i,4).value,
-            ws.cell(i,5).value,
-            ws.cell(i,6).value,
-            str(ws.cell(i,7).value),
-            ws.cell(i,8).value)
-        if(s[1] in str(ws.cell(i,2).value) and n == 1):
-            s2 = Squadra(str(ws.cell(i,2).value),
-            ws.cell(i,3).value,
-            ws.cell(i,4).value,
-            ws.cell(i,5).value,
-            ws.cell(i,6).value,
-            str(ws.cell(i,7).value),
-            ws.cell(i,8).value)
-s1.stampa()
-s2.stampa()
+i=offset
+n = 0
+while i < 21 and n<2:
+    i = i + 1
+    if(str(s[n]) in str(ws.cell(i,2).value)):
+        n=n+1
+        sq.append(Squadra(str(ws.cell(i,2).value),
+        ws.cell(i,3).value,
+        ws.cell(i,4).value,
+        ws.cell(i,5).value,
+        ws.cell(i,6).value,
+        str(ws.cell(i,7).value),
+        ws.cell(i,8).value))
+    if(i==21 and n<2):
+        i=0
+'''
+for el in sq:
+    print(el)'''
 print("\n\n")
 
+
 #Confronto dei punti:
-if s1.pt>s2.pt: bil=bil-1
-if s1.pt<s2.pt: bil=bil+1
+if sq[0].pt>sq[1].pt: bil=bil-1
+if sq[0].pt<sq[1].pt: bil=bil+1
 #Confronto dei gol
-if s1.gol()>s2.gol(): bil=bil-1
-if s1.gol()<s2.gol(): bil=bil+1
+if sq[0].gol()>sq[1].gol(): bil=bil-1
+if sq[0].gol()<sq[1].gol(): bil=bil+1
 print(bil)
 print("Dati aggiornati al " + str(ws.cell(1,10).value))
